957227fa4edf9e1edb4c3c7bda53d4193b0652ab
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-01 17:12:43 -0400

    added imports to components into DataArea

---
+++ src/components/DataArea.js
@@ -0,0 +1,7 @@
+import React from "react";
+import DataBody from "./DataBody";
+import DataTable from "./DataTable";
+import Header from "./Header";
+import Nav from "./Nav";
+import SearchBox from "./SearchBox";
+

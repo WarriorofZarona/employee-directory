50c27151a8afeab1a6a0efc741d69af0224fd434
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-01 17:16:07 -0400

    added API import

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -4,6 +4,7 @@ import DataTable from "./DataTable";
 import Header from "./Header";
 import Nav from "./Nav";
 import SearchBox from "./SearchBox";
+import API from "../utils/API"
 
 class DataArea extends Component {
 

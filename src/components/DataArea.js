42fbe4d1d678caf38c8c1232bf298ebd3948885a
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-01 17:15:09 -0400

    added DataArea starting code and export

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import DataBody from "./DataBody";
 import DataTable from "./DataTable";
 import Header from "./Header";
 import Nav from "./Nav";
 import SearchBox from "./SearchBox";
 
+class DataArea extends Component {
+
+
+}
+
+export default DataArea;

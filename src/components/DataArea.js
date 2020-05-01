fc0094f1310303319a391e956f637291d9a668fd
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-01 18:06:34 -0400

    added Hello to DataArea to test the render, starting to code the table itself and a bit of the nav bar

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -3,12 +3,15 @@ import DataTable from "./DataTable";
 import Header from "./Header";
 import Nav from "./Nav";
 import SearchBox from "./SearchBox";
-import API from "../utils/API"
 
 class DataArea extends Component {
     render() {
         return (
-            <DataTable />
+            <div>
+                <Nav />
+                <p>Hello</p>
+                <DataTable />
+            </div>
         )
     }
 }
---
+++ src/components/DataBody.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+function DataBody(props) {
+
+
+}

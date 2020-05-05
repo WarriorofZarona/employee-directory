2ba1341183f7e0ef2d945274931af8c95e38909e
Andrew Bautista <andrewbautista85@gmail.com>
2020-05-05 19:05:03 -0400

    added text explaining sort function

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -8,7 +8,8 @@ class PageHeader extends Component {
         return (
             <div className="jumbotron text-center" >
                 <h1 className="display-4">Employee Directory</h1>
-                <p className="lead">Type in search bar below for an employee's name</p>
+                <p className="lead">Type in search bar below for an employee's name.</p>
+                <p className="lead">Click the table header to toggle names in ascending/descending order.</p>
                 <hr className="my-4" />
                 <SearchBox handler={this.props.handler} />
             </div>
